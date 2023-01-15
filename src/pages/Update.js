0cc7c022b9deb78fcf56ab13319287b4966d31c6
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-14 19:53:22 -0500

    added an upadate page

---
+++ src/pages/Update.js
@@ -0,0 +1,18 @@
+import { useLoaderData, Form } from "react-router-dom"
+
+function Update(props) {
+  const bookmark = useLoaderData()
+
+  return (
+    <div className="person">
+      <h2>Update {bookmark.title}</h2>
+      <Form action={`/update/${bookmark._id}`} method="post">
+        <input defaultValue={bookmark.title} type="input" name="title" placeholder="Site title" />
+        <input defaultValue={bookmark.url} type="input" name="url" placeholder="Site url" />
+        <input type="submit" value={`Update ${bookmark.title}`} />
+      </Form>
+    </div>
+  )
+}
+
+export default Update
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 import App from "../App"
 import Index from "../pages/Index"
+import Update from "../pages/Update"
 import { indexLoader } from "./loaders"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={indexLoader} />
+      <Route path=":id" element={<Update />} />
     </Route>
   )
 )

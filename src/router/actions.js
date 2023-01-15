2053606779089b9640856ff2809fdb4e28a375d6
Brandon Haupt <haupt.brandon@gmail.com>
2023-01-14 19:16:22 -0600

    Added UpdateAction to Actions.js and changed Router.js

---
+++ src/router/actions.js
@@ -0,0 +1,21 @@
+import { redirect } from "react-router-dom";
+
+const URL = "https://bookmarkd-backend-hhwq.onrender.com"
+
+export const updateAction = async ({request, params}) => {
+    const formData = await request.formData()
+
+    const updatedBookmark = {
+        title: formData.get("title"),
+        url: formData.get('url')
+    }
+
+    await fetch(URL + "/bookmarks/" + params.id, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedBookmark)
+    })
+    return redirect("/")
+}
--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,13 @@ import App from "../App"
 import Index from "../pages/Index"
 import Update from "../pages/Update"
 import { indexLoader } from "./loaders"
+import { updateAction } from "./actions"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={indexLoader} />
-      <Route path=":id" element={<Update />} />
+      <Route path=":id" element={<Update />} action={updateAction}/>
     </Route>
   )
 )

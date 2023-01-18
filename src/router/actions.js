404f3bc4c7ea7d8d7aaedb86243f93361a0cc878
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-17 17:26:21 -0700

    adds create action to actions and router

--- src/router/actions.js
+++ src/router/actions.js
@@ -2,6 +2,27 @@ import { redirect } from "react-router-dom"
 
 const URL = "https://bookmarkd-backend-hhwq.onrender.com"
 
+//Create Action
+export const createAction = async ({ request }) => {
+    const formData = await request.formData();
+
+    const newBookmark = {
+        title: formData.get("title"),
+        url: formData.get("url"),
+        
+    };
+    
+    await fetch(URL + '/bookmarks', {
+        method: "post",
+        headers: {
+            "Content-Type": "application.json",
+        },
+        body: JSON.stringify(newBookmark),
+    });
+
+    return redirect('/');
+};
+
 // Update Action
 export const updateAction = async ({ request, params }) => {
   const formData = await request.formData()
